e215bef394c7d0857f9f2369a398d7a293b2c4ce
mfrag2025 <59969280+mfrag2025@users.noreply.github.com>
2022-03-28 00:38:05 -0400

    Update authentication.php

--- authentication.php
+++ authentication.php
@@ -5,11 +5,11 @@ session_start();
     $username = $_POST['user'];  
     $password = $_POST['pass'];  
       
-        //to prevent from mysqli injection  
-        //$username = stripcslashes($username);  
-        //$password = stripcslashes($password);  
-        //$username = mysqli_real_escape_string($con, $username);  
-        //$password = mysqli_real_escape_string($con, $password);  
+        to prevent from mysqli injection  
+        $username = stripcslashes($username);  
+        $password = stripcslashes($password);  
+        $username = mysqli_real_escape_string($con, $username);  
+        $password = mysqli_real_escape_string($con, $password);  
       
         $sql = "select * from users where username = '$username' and password = '$password'";  
         $result = mysqli_query($con, $sql);  
